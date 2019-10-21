7ef04e4e8a881bb069b42ade64c6630db170d231
YuppieJ <1171065420@qq.com>
2019-10-21 23:23:25 +0800

    把本地工程添加到git

---
+++ first-git-demo-2/src/com/wwj/FirstClass.java
@@ -0,0 +1,15 @@
+package com.wwj;
+
+/**
+ * @Author: Yuppie❤ N
+ * @Date: Create in 23:21 2019/10/21
+ * @Description:
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello first project dome-1");
+        System.out.println("hello first project dome-1");
+        System.out.println("hello first project dome-1");
+    }
+}
