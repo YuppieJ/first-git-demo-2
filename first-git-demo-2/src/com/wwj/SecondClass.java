ec099e3fdea0f29dccff5e93e206ae5e23ff90fd
YuppieJ <1171065420@qq.com>
2019-10-21 23:51:08 +0800

    SecondClass添加了

---
+++ first-git-demo-2/src/com/wwj/SecondClass.java
@@ -0,0 +1,14 @@
+package com.wwj;
+
+/**
+ * @Author: Yuppie❤ N
+ * @Date: Create in 23:50 2019/10/21
+ * @Description:
+ */
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("hello first project dome-2");
+        System.out.println("hello first project dome-2");
+        System.out.println("hello first project dome-2");
+    }
+}
